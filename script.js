28d5351b1786bd7929c00377ec784b2fdce0b6fc
AbdulSami-786 <abdulsamimyousuf0@gmail.com>
2025-02-21 01:03:34 +0500

    website wit full responsive

--- script.js
+++ script.js
@@ -5,39 +5,33 @@ let div = document.querySelector(".container")
 let sub = document.querySelector(".submit")
 let form = document.querySelector(".form")
 form.addEventListener("submit",(event)=>{
-event.preventDefault()
-fetch(`https://api.github.com/users/${user.value}`)
-.then((res)=>{
-    return res.json()
-})
-.then((re)=>{
-   console.log(re);
-   userarr.unshift(re)
-   render()
-//    div.innerHTML = ` <div class="user">
-//         <div class="img">
-//             <img src="${re.avatar_url}" alt="">
-//         </div>
-//         <div class="item">
-//             <h1>Name : ${re.name}</h1>
-//             <h2>Bio : ${re.bio}</h2>
-//             <h3>
-//                 <b class="b1">follower : ${re.followers}</b>
-//                 <b class="b2">following : ${re.following}</b>
-//             </h3>
-//             <h4>${re.created_at}</h4>
-//             <h5>${re.login}</h5>
-//         </div>
-//     </div>`
+    sub.value = "Loading..."
+    event.preventDefault()
+    fetch(`https://api.github.com/users/${user.value}`)
+    .then((res)=>{
+        return res.json()
+    })
+    .then((re)=>{
+    user.value = ""
+    console.log(re);
+    userarr.unshift(re)
+    if(re.id){
+        render()
+}
+else{
+    alert("user Not found")
+    userarr.shift()
+    render()
+}
 })
 .catch((err)=>{
-return err
+    return err
 })
 .catch((er)=>{
     console.log(er);
-alert("User not found")
-userarr.shift()
-render()
+})
+.finally(()=>{
+sub.value = "Chek User"
 })
 })
 function render() {
